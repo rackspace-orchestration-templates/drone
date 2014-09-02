5e17dd3673e1a25a7d09e12d8794b2a77b09cac8
Brint O'Hearn <brint.ohearn@rackspace.com>
2014-09-02 13:18:00 -0500

    Adding test to make sure drone is properly responding

--- test/fabric/drone_server.py
+++ test/fabric/drone_server.py
@@ -1,5 +1,19 @@
-from fabric.api import env, task
+# coding=utf8
+
+import re
+
+from fabric.api import env, hide, run, task
 from envassert import detect, file, package, port, process, service
+
+
+def drone_is_responding():
+    with hide('running', 'stdout'):
+        site = "https://localhost/"
+        homepage = run("wget --quiet --output-document - --no-check-certificate %s" % site)
+        if re.search('Login · drone.io', homepage):
+            return True
+        else:
+            return False
 
 
 @task
@@ -13,3 +27,4 @@ def check():
     assert port.is_listening(443)
     assert process.is_up("droned")
     assert service.is_enabled("drone")
+    assert drone_is_responding()
